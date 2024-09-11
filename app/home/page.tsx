4d42b226fae9115b469a6a1a5ec51b2b833b4f4d
Kim Robinson <kimmykokonut@gmail.com>
2024-09-11 13:45:34 -0700

    style home page buttons

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,13 +3,17 @@
 const Hero = () => {
   return (
     <section id="home" role="region" aria-labelledby="hero-heading">
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Kim Robinson</h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl">words and stuff and things....</p>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">Kim Robinson</h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">words and stuff and things....</p>
+          <div>
+            <button className="px-4 py-2 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 bg-gradient-to-br from-blue-500 via-green-500 to-yellow-500 text-white">GitHub</button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-green-500 to-yellow-500 hover:bg-slate-800 "><span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-3 py-1 text-white">LinkedIn</span></button>
+          </div>
         </div>
-        <div className="col-span-5">
-            <img className="rounded-full mx-auto p-3 h-[388px] w-[300px] md:h-[550px] md:w-[420px]" src="./static/hunt.jpg" alt="person standing in the forest" />
+        <div className="col-span-5 mt-4 lg:mt-0">
+            <img className="object-cover rounded-full mx-auto p-3 h-[388px] w-[300px] md:h-[550px] md:w-[420px]" src="./static/hunt.jpg" alt="person standing in the forest" />
         </div>
       </div>
     </section>
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] mx-auto px-12 py-4">
       <Hero />
-      <About />
       <Projects />
       <Skills />
       <Contact />
