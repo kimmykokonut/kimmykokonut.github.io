16baec4b55feff211ebf41e286af7f36bc5d33b4
Kim Robinson <kimmykokonut@gmail.com>
2024-10-01 17:00:36 -0700

    update styling for home page and change buttons to links

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { TypeAnimation } from 'react-type-animation';
+import Image from 'next/image';
 
 const Hero = () => {
   const [isClient, setIsClient] = useState(false);
@@ -14,7 +15,7 @@ const Hero = () => {
     <section id="home" role="region" aria-labelledby="hero-heading">
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+          <h1 className="text-slate-800 dark:text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             Kim Robinson
           </h1>
 
@@ -39,12 +40,25 @@ const Hero = () => {
             )}
           </p>
           <div>
-            <button className="px-4 py-2 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 bg-gradient-to-br from-blue-500 via-green-500 to-yellow-500 text-white">GitHub</button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-green-500 to-yellow-500 hover:bg-slate-800 "><span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-3 py-1 text-white">LinkedIn</span></button>
+            <a
+              href='https://github.com/kimmykokonut'
+              className="px-4 py-2 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 bg-gradient-to-br from-blue-500 via-green-500 to-yellow-500 text-white inline-block text-center">GitHub
+            </a>
+            <a
+              href='https://www.linkedin.com/in/robinson-kim/'
+              className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-green-500 to-yellow-500 hover:bg-slate-800 inline-block text-center">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-3 py-1 text-white">LinkedIn</span>
+            </a>
           </div>
         </div>
         <div className="col-span-5 mt-4 lg:mt-0">
-          <img className="object-cover rounded-full mx-auto p-3 h-[388px] w-[300px] md:h-[550px] md:w-[420px]" src="./static/hunt.jpg" alt="person standing in the forest" />
+          <Image
+            className="object-cover border-solid border-slate-800 rounded-full mx-auto p-3 h-[388px] w-[300px] md:h-[550px] md:w-[420px]"
+            src="/static/hunt.jpg"
+            alt="person standing in the forest"
+            width={300}
+            height={500}
+          />
         </div>
       </div>
     </section>
