77248f9f98e7c07b46e3b5d4f8e46c81ff6b5508
Kim Robinson <kimmykokonut@gmail.com>
2024-10-18 14:31:06 -0700

    add ts interface for tabbutton

--- components/TabButton.tsx
+++ components/TabButton.tsx
@@ -1,4 +1,12 @@
-const TabButton = ({ active, selectTab, children }) => {
+import { ReactNode } from "react";
+
+interface TabButtonProps {
+  active: boolean;
+  selectTab: () => void;
+  children: ReactNode;
+}
+
+const TabButton: React.FC<TabButtonProps> = ({ active, selectTab, children }) => {
   //hover is active to show text color change
   const buttonClasses = active ? 'text-green-700 dark:text-white border-b border-green-700' : 'text-[#121212] dark:text-[#ADB7BE]'
 
