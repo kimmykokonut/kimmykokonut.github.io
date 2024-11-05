42cfd5aa64c4cc1952e6a76c05a5c40046bd58ef
Kim Robinson <kimmykokonut@gmail.com>
2024-11-05 11:30:49 -0800

    fix bug with scroll snap adding visual padding right

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Contact from "./contact/page";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#f2f3f4] dark:bg-[#1e1e1e] mx-auto px-12 py-4">
-      <div className="container mt-24 mx-auto">
+      <div className="container mt-24 mx-auto overflow-hidden">
         <Hero />
         <About />
         <Projects />
